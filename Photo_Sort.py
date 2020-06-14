4f98f0d69bc81e59364b2b9d3c4eaa6d57a21680
Joe Jenkins <jjenkins@Mantaray.local>
2020-06-13 18:11:20 -0700

    initial commit with pseudo code

---
+++ Photo_Sort.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+
+DIRECTORY = input("What is the name of the Directory? ")
+
+# ask for the name of the directory
+
+# create a directory for jpg
+
+# create a directory for raw files
+
+# rename the files based on the requested directory name
+
+# rename the files based on creation date?
+
+# rename the files with a custom date?
